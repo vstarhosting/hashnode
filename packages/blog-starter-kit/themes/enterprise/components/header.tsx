8eeabacba108b58c6bb1732077d3c004b9d029eb
vstarhosting <71176062+vstarhosting@users.noreply.github.com>
2024-07-16 23:21:12 +0530

    Update header.tsx
    new header

--- packages/blog-starter-kit/themes/enterprise/components/header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/header.tsx
@@ -9,99 +9,111 @@ import { PublicationLogo } from './publication-logo';
 import PublicationSidebar from './sidebar';
 
 function hasUrl(
-	navbarItem: PublicationNavbarItem,
+  navbarItem: PublicationNavbarItem,
 ): navbarItem is PublicationNavbarItem & { url: string } {
-	return !!navbarItem.url && navbarItem.url.length > 0;
+  return !!navbarItem.url && navbarItem.url.length > 0;
 }
 
 export const Header = () => {
-	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '/';
-	const [isSidebarVisible, setIsSidebarVisible] = useState<boolean>();
-	const { publication } = useAppContext();
-	const navbarItems = publication.preferences.navbarItems.filter(hasUrl);
-	const visibleItems = navbarItems.slice(0, 3);
-	const hiddenItems = navbarItems.slice(3);
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '/';
+  const [isSidebarVisible, setIsSidebarVisible] = useState<boolean>();
+  const { publication } = useAppContext();
+  const navbarItems = publication.preferences.navbarItems.filter(hasUrl);
+  const visibleItems = navbarItems.slice(0, 3);
+  const hiddenItems = navbarItems.slice(3);
 
-	const toggleSidebar = () => {
-		setIsSidebarVisible((prevVisibility) => !prevVisibility);
-	};
+  const toggleSidebar = () => {
+    setIsSidebarVisible((prevVisibility) => !prevVisibility);
+  };
 
-	const navList = (
-		<ul className="flex flex-row items-center gap-2 text-white">
-			{visibleItems.map((item) => (
-				<li key={item.url}>
-					<a
-						href={item.url}
-						target="_blank"
-						rel="noopener noreferrer"
-						className="transition-200 block max-w-[200px] truncate text-ellipsis whitespace-nowrap rounded-full p-2 transition-colors hover:bg-white hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white"
-					>
-						{item.label}
-					</a>
-				</li>
-			))}
+  const navList = (
+    <ul className="flex flex-row items-center gap-4 text-white">
+      {visibleItems.map((item) => (
+        <li key={item.url}>
+          <a
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-200 block max-w-[200px] truncate text-ellipsis whitespace-nowrap rounded-full p-2 transition-colors hover:bg-white hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white"
+          >
+            {item.label}
+          </a>
+        </li>
+      ))}
 
-			{hiddenItems.length > 0 && (
-				<li>
-					<DropdownMenu.Root>
-						<DropdownMenu.Trigger asChild>
-							<button className="transition-200 block rounded-full p-2 transition-colors hover:bg-white hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white">
-								More
-							</button>
-						</DropdownMenu.Trigger>
+      {hiddenItems.length > 0 && (
+        <li>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild>
+              <button className="transition-200 block rounded-full p-2 transition-colors hover:bg-white hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white">
+                More
+              </button>
+            </DropdownMenu.Trigger>
 
-						<DropdownMenu.Portal>
-							<DropdownMenu.Content
-								className="w-48 rounded border border-gray-300 bg-white text-neutral-950 shadow-md dark:border-neutral-800 dark:bg-neutral-900 dark:text-white"
-								align="end"
-								sideOffset={5}
-							>
-								{hiddenItems.map((item) => (
-									<DropdownMenu.Item asChild key={item.url}>
-										<a
-											href={item.url}
-											target="_blank"
-											rel="noopener noreferrer"
-											className="transition-200 block truncate p-2 transition-colors hover:bg-slate-100 hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white"
-										>
-											{item.label}
-										</a>
-									</DropdownMenu.Item>
-								))}
-							</DropdownMenu.Content>
-						</DropdownMenu.Portal>
-					</DropdownMenu.Root>
-				</li>
-			)}
-		</ul>
-	);
+            <DropdownMenu.Portal>
+              <DropdownMenu.Content
+                className="w-48 rounded border border-gray-300 bg-white text-neutral-950 shadow-md dark:border-neutral-800 dark:bg-neutral-900 dark:text-white"
+                align="end"
+                sideOffset={5}
+              >
+                {hiddenItems.map((item) => (
+                  <DropdownMenu.Item asChild key={item.url}>
+                    <a
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="transition-200 block truncate p-2 transition-colors hover:bg-slate-100 hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white"
+                    >
+                      {item.label}
+                    </a>
+                  </DropdownMenu.Item>
+                ))}
+              </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+          </DropdownMenu.Root>
+        </li>
+      )}
+    </ul>
+  );
 
-	return (
-		<header className="border-b bg-slate-950 py-10 dark:border-neutral-800 dark:bg-neutral-900">
-			<Container className="grid grid-cols-4 gap-5 px-5">
-				<div className="col-span-2 flex flex-1 flex-row items-center gap-2 lg:col-span-1">
-					<div className="lg:hidden">
-						<Button
-							type="outline"
-							label=""
-							icon={<HamburgerSVG className="h-5 w-5 stroke-current" />}
-							className="rounded-xl border-transparent !px-3 !py-2 text-white hover:bg-slate-900 dark:hover:bg-neutral-800"
-							onClick={toggleSidebar}
-						/>
+  return (
+    <header className="border-b bg-slate-950 py-5 dark:border-neutral-800 dark:bg-neutral-900">
+      <Container className="grid grid-cols-4 gap-5 px-5">
+        <div className="col-span-2 flex flex-1 flex-row items-center gap-2 lg:col-span-1">
+          <div className="lg:hidden">
+            <Button
+              type="outline"
+              label=""
+              icon={<HamburgerSVG className="h-5 w-5 stroke-current" />}
+              className="rounded-xl border-transparent !px-3 !py-2 text-white hover:bg-slate-900 dark:hover:bg-neutral-800"
+              onClick={toggleSidebar}
+            />
 
-						{isSidebarVisible && (
-							<PublicationSidebar navbarItems={navbarItems} toggleSidebar={toggleSidebar} />
-						)}
-					</div>
-					<div className="hidden lg:block">
-						<PublicationLogo />
-					</div>
-				</div>
-			
-			</Container>
-			<div className="mt-5 flex justify-center lg:hidden">
-				<PublicationLogo />
-			</div>
-		</header>
-	);
+            {isSidebarVisible && (
+              <PublicationSidebar navbarItems={navbarItems} toggleSidebar={toggleSidebar} />
+            )}
+          </div>
+          <div className="hidden lg:block">
+            <PublicationLogo />
+          </div>
+        </div>
+
+        <nav className="col-span-2 hidden lg:flex justify-center">
+          {navList}
+        </nav>
+
+        <div className="col-span-1 hidden lg:flex justify-end items-center">
+          <Button
+            type="outline"
+            label="Subscribe"
+            className="rounded-full border-white text-white hover:bg-white hover:text-black dark:hover:bg-neutral-800 dark:hover:text-white"
+          />
+        </div>
+      </Container>
+
+      <div className="mt-5 flex justify-center lg:hidden">
+        <PublicationLogo />
+      </div>
+    </header>
+  );
 };
