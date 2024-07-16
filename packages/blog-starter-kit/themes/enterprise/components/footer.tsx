de2db0c0c79513d356fadb2d17ef42c47f54b2ad
vstarhosting <71176062+vstarhosting@users.noreply.github.com>
2024-07-16 23:45:21 +0530

    Update footer.tsx

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -4,42 +4,42 @@ import { useAppContext } from './contexts/appContext';
 import { SocialLinks } from './social-links';
 
 export const Footer = () => {
-  const { publication } = useAppContext();
-  const PUBLICATION_LOGO = publication.preferences.logo;
-  const currentYear = new Date().getFullYear();
+	const { publication } = useAppContext();
+	const PUBLICATION_LOGO = publication.preferences.logo;
+	const currentYear = new Date().getFullYear();
 
-  return (
-    <footer className="border-t py-10 dark:border-neutral-800 bg-gray-50 dark:bg-gray-900">
-      <Container className="flex flex-col items-center px-5 text-center">
-        {PUBLICATION_LOGO ? (
-          <div className="mb-5">
-            <Link
-              href={'/'}
-              aria-label={`${publication.title} home page`}
-              className="flex justify-center"
-            >
-              <img className="block w-40" src={PUBLICATION_LOGO} alt={publication.title} />
-            </Link>
-          </div>
-        ) : (
-          <p className="mb-5 text-xl font-semibold text-slate-900 dark:text-slate-50 md:text-2xl">
-            {publication.title}
-          </p>
-        )}
-        <SocialLinks className="mb-5" />
-        <p className="text-slate-600 dark:text-neutral-300">
-          &copy; {currentYear} BestinSG. All rights reserved.
-        </p>
-        <p className="text-slate-600 dark:text-neutral-300">
-          <Link href="/privacy-policy" className="hover:underline">
-            Privacy Policy
-          </Link>{' '}
-          ·{' '}
-          <Link href="/terms" className="hover:underline">
-            Terms
-          </Link>
-        </p>
-      </Container>
-    </footer>
-  );
+	return (
+		<footer className="border-t py-10 dark:border-neutral-800 bg-gray-50 dark:bg-gray-900">
+			<Container className="flex flex-col items-center px-5 text-center">
+				{PUBLICATION_LOGO ? (
+					<div className="mb-5">
+						<Link
+							href={'/'}
+							aria-label={`${publication.title} home page`}
+							className="flex justify-center"
+						>
+							<img className="block w-40" src={PUBLICATION_LOGO} alt={publication.title} />
+						</Link>
+					</div>
+				) : (
+					<p className="mb-5 text-xl font-semibold text-slate-900 dark:text-slate-50 md:text-2xl">
+						{publication.title}
+					</p>
+				)}
+				<SocialLinks className="mb-5" />
+				<p className="text-slate-600 dark:text-neutral-300">
+					&copy; {currentYear} BestinSG. All rights reserved.
+				</p>
+				<p className="text-slate-600 dark:text-neutral-300">
+					<Link href="/privacy-policy" className="hover:underline">
+						Privacy Policy
+					</Link>{' '}
+					·{' '}
+					<Link href="/terms" className="hover:underline">
+						Terms
+					</Link>
+				</p>
+			</Container>
+		</footer>
+	);
 };
